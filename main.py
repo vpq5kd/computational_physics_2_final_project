4333d24b20023a1e20548d3a883e6afbd7585da3
sophia spaner <vpq5kd@virginia.edu>
2026-04-28 23:21:21 -0400

    got basic stemi separation and my overall dataset

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import pandas as pd
 from classes.ecg_viewer import ecg_viewer
+from pathlib import Path
 
+PTBXL_ROOT = Path("~/ecg_classification/PTB-XL/").expanduser()
+df_stemi = pd.read_csv("persisted_data/stemi_filenames.csv")
+stemi_filenames = [str(PTBXL_ROOT/filename) for filename in df_stemi["filename_lr"]]
 
+ev = ecg_viewer()
+
+for ecg in stemi_filenames:
+    ev.view_ecg(ecg)
+    
